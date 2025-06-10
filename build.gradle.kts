b6e7d25aa58d3654d3e1b9a1dc097a54fc2ea8c3
Phan Ngọc Tài <21130164@st.hcmuaf.edu.vn>
2025-06-10 14:47:39 +0700

    Điều chỉnh không hardcode host và port trong build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,54 @@
+val javaVersion = JavaVersion.VERSION_21
+val lombokVersion = "1.18.32"
+val mapstructVersion = "1.6.3"
+val lombokMapstructBindingVersion = "0.2.0"
+val springDocVersion = "2.8.8"
+val graphqlPlaygroundVersion = "1.6.30"
+val cloudinaryVersion = "1.38.0"
+val cloudinaryTaglibVersion = "1.36.0"
+val dotenvVersion = "2.2.4"
+val mysqlConnectorVersion = "9.3.0"
+val testNgVersion = "7.10.2"
+val springWebfluxVersion = "6.1.14"
+val apacheCommonsVersion = "3.12.0"
+val apachePoiVersion = "5.4.1"
+val jBcryptVersion = "0.4"
+val graphQLExtendScalarsVersion = "22.0"
+
 plugins {
     java
-    id("org.springframework.boot") version "3.5.0"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("org.asciidoctor.jvm.convert") version "3.3.2"
-    id("com.diffplug.spotless") version "7.0.4"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.asciidoctor.jvm.convert")
+    id("com.diffplug.spotless")
 }
 
 group = "vn.edu.hcmuaf.fit"
 version = "0.0.1-SNAPSHOT"
 
+
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(javaVersion.majorVersion.toInt()))
     }
 }
+
+
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
+
 repositories {
     mavenCentral()
 }
 
-// Định nghĩa biến snippetsDir từ extra properties
-val snippetsDir: File by extra(file("${buildDir.path}/generated-snippets"))
+
+val snippetsDir = layout.buildDirectory.dir("generated-snippets")
 
 dependencies {
-    // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -36,62 +56,45 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
     implementation("org.springframework.boot:spring-boot-starter-mail")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
-
-
-    // Core libraries
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("org.springframework:spring-context-support")
     implementation("org.springframework.security:spring-security-crypto")
     implementation("jakarta.persistence:jakarta.persistence-api")
-    implementation("org.springframework:spring-webflux:6.1.14")
+    implementation("org.springframework:spring-webflux:$springWebfluxVersion")
+    implementation("com.cloudinary:cloudinary-http44:$cloudinaryVersion")
+    implementation("com.cloudinary:cloudinary-taglib:$cloudinaryTaglibVersion")
+    implementation("io.github.cdimascio:dotenv-java:$dotenvVersion")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
+    implementation("org.webjars.npm:graphql-playground-html:$graphqlPlaygroundVersion")
+    implementation("com.graphql-java:graphql-java-extended-scalars:$graphQLExtendScalarsVersion")
+    implementation("org.apache.commons:commons-lang3:$apacheCommonsVersion")
+    implementation("org.mindrot:jbcrypt:$jBcryptVersion")
+    implementation("org.apache.poi:poi:$apachePoiVersion")
+    implementation("org.apache.poi:poi-ooxml:$apachePoiVersion")
 
-    // Lombok & MapStruct
-    val lombokVersion = "1.18.32"
-    val mapstructVersion = "1.6.3"
     compileOnly("org.projectlombok:lombok:$lombokVersion")
+    compileOnly("org.mapstruct:mapstruct:$mapstructVersion")
+
     annotationProcessor("org.projectlombok:lombok:$lombokVersion")
-    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:$lombokMapstructBindingVersion")
     annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
-
-    // Database drivers
-    runtimeOnly("com.mysql:mysql-connector-j")
-    testImplementation("com.h2database:h2:2.2.224")
-
-    // Cloudinary & dotenv
-    implementation("com.cloudinary:cloudinary-http44:1.38.0")
-    implementation("com.cloudinary:cloudinary-taglib:1.36.0")
-    implementation("io.github.cdimascio:dotenv-java:2.2.4")
-
-    // OpenAPI & GraphQL UI
-    // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
-    implementation("org.webjars.npm:graphql-playground-html:1.6.30")
-    implementation("com.graphql-java:graphql-java-extended-scalars:22.0")
-
-    // Other third-party
-    implementation("org.mindrot:jbcrypt:0.4")
-    implementation("org.apache.poi:poi:5.4.1")
-    implementation("org.apache.poi:poi-ooxml:5.4.1")
-
-    // Devtools & Docker Compose
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    // Spring configuration processor
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-    // Testing
+    runtimeOnly("com.mysql:mysql-connector-j:$mysqlConnectorVersion")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.graphql:spring-graphql-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.testng:testng:7.10.2")
+    testImplementation("org.testng:testng:$testNgVersion")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
